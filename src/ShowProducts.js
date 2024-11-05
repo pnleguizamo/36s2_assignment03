5362d6ccb677cd77e0a683f813170f27178c4bf0
pnleguizamo <pnleguizamo@gmail.com>
2024-11-04 19:10:24 -0600

    Cleanup ShowProducts

--- src/ShowProducts.js
+++ src/ShowProducts.js
@@ -6,59 +6,7 @@ import Payment from './PaymentInfo';
 
 
 function ShowProducts({ cart, setCart, cartTotal, setCartTotal, dataF, setDataF, viewer, setViewer }) {
-  // const [catalog, setCatalog] = useState([]);
-  // const [filteredCatalog, setFilteredCatalog] = useState([]);
-  // const [categories, setCategories] = useState([]);
-  // const [searchTerm, setSearchTerm] = useState('');
-  // const [cart, setCart] = useState([]);
-
-
-  const fetchData = async () => {
-    const response = await fetch("/products.json");
-    const data = await response.json();
-
-    // setCart(data);
-
-    // const responseCategories = await fetch("/categories.json");
-    // const dataCategories = await responseCategories.json();
-    // setCategories(dataCategories);
-    // console.log(dataCategories);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, [])
-
-  function howManyofThis(id) {
-    let hmot = cart.filter((cartItem) => cartItem.id === id);
-    return hmot.length;
-  }
-
-  const getUniqueProductsWithQuantities = (cart) => {
-    const uniqueProducts = {};
-
-    cart.forEach(product => {
-      if (!uniqueProducts[product.id]) {
-        uniqueProducts[product.id] = { ...product, quantity: product.quantity };
-      } else {
-        uniqueProducts[product.id].quantity += product.quantity;
-      }
-    });
-
-
-    return Object.values(uniqueProducts);
-  };
-
-  const cartItems = cart.map((el, index) => (
-    <div key={index}>
-      <img className="img-fluid" src={el.image} width={150} alt={el.title} />
-      {el.title} ${el.price} x {el.quantity}
-    </div>
-  ));
-
-  const uniqueProductsArray = getUniqueProductsWithQuantities(cart);
-
-
+  
   return (
     <div >
       <div className="table-responsive small">
@@ -78,7 +26,7 @@ function ShowProducts({ cart, setCart, cartTotal, setCartTotal, dataF, setDataF,
                   <h3>{product.title}</h3>
                 </td>
                 <td>
-                  {howManyofThis(product.id)}
+                  {product.quantity}
                 </td>
                 <td>
                   {product.price}
